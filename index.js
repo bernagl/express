9ba8debf5938487ca8354fb5ddf54622238cd725
Luis García <luisgarcia@Luiss-MBP.lan>
2018-01-10 03:15:21 -0600

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({ hi: 'thereeee' })
+  res.send({ hi: 'Hola' })
 })
 
 // process.env.PORT get the port of the environment production
