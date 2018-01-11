c1fc186256c2ac7110a34466c8c626afef11ec65
Luis García <luisgarcia@Luiss-MBP.lan>
2018-01-11 16:18:01 -0600

    fix: env keys

---
+++ config/keys.js
@@ -0,0 +1,18 @@
+// dev
+// 17267368939-1qj2f4du3foua4ue8r8lj8fjgnokr9g1.apps.googleusercontent.com
+// uCCM-QCk_gH8ry6ncLjXqbOQ
+
+// module.exports = {
+//   googleClientID:
+//     '625861011496-sqktrr645q5bo54lcdiaabficqkj4sfo.apps.googleusercontent.com',
+//   googleClientSecret: '8Kiolx-i_0bO2H9EHv1qoKUK',
+//   mongoURI:
+//     'mongodb://admin:admin@ds249707.mlab.com:49707/express-server-dev',
+//     cookieKey: 'uid'
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prods')
+} else {
+  module.exports = require('./dev')
+}
