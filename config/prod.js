046a3650beb4c05585da0f6589bbeb6465b22d4f
Luis García <luisgarcia@Luiss-MBP.lan>
2018-01-11 16:10:32 -0600

    Add: Keys environment

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
+
+// module.exports = {
+//   googleClientID:
+//     '17267368939-1qj2f4du3foua4ue8r8lj8fjgnokr9g1.apps.googleusercontent.com',
+//   googleClientSecret: 'uCCM-QCk_gH8ry6ncLjXqbOQ',
+//   mongoURI: 'mongodb://admin:admin@ds249787.mlab.com:49787/express-server',
+//   cookieKey: 'uid'
+// }
